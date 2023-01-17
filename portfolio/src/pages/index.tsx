9e27365bb5473baa5349cf16149508737da6a800
vithya27 <vithya.shankar93@hotmail.com>
2023-01-17 16:31:51 +0800

    Create folders and setup Tailwind CSS

---
+++ portfolio/src/pages/index.tsx
@@ -0,0 +1,28 @@
+import Head from "next/head";
+import { Inter } from "@next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Vithya's portfolio</title>
+      </Head>
+
+      {/* Header */}
+
+      {/* Hero */}
+
+      {/* About */}
+
+      {/* Experience */}
+
+      {/* Skills */}
+
+      {/* Projects */}
+
+      {/* Contact Me */}
+    </>
+  );
+}
