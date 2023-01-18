0f6a82bf84b39c26ac63a8053ea436495202b4ef
vithya27 <vithya.shankar93@hotmail.com>
2023-01-18 11:00:29 +0800

    Updated image link

--- portfolio/components/About.tsx
+++ portfolio/components/About.tsx
@@ -19,7 +19,7 @@ function About({}: Props) {
         viewport={{ once: true }}
         transition={{ duration: 1.2 }}
         className="-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-95 xl:w-[500px] xl:h-[600px]"
-        src="https://lh3.googleusercontent.com/2h6JCdsisvTpniw05z4gTK51HK4PskO0jvYUnJ79CMoX-NUsTBmF2R29I4p2MbApJeC-vG1MFsIYzbe8RqHLPvTuw-oWYM-tHYXLKn8NXOhuT7Pl59BagWE0qwqmWoK9PSfjkXJqL6z82mNZg4kyT8ofgNwnA1C9rtq3Uk64o8_FQyUFc3cE9pZ5N8ESbQEm9ii-MWdpAkgZNV7YI_eW_piMvl2Y8a_b8Ml4VPiiJbE-sf3JiuKQStqRtCJkYnmyHNvTRS2kLqd0bRSi7quJC3NmN-2YAZVdemUgLo8yEwGXHcp_zHlyrdgGDr8jabRthvJ81bBjBD6K7ZyNEerq8bpC5To4FM09SRVmO4pKcPq2C6ss3wMCNA-PshRoZ-8-yuoDSV3j0RpISjejDAZmlQpDKrYtIBk29wY6ntS6wVC5vxR1l51hsD99jNikHhKVKu81SBVtWzy7Euoisfj_J2ca2Kck9dyrwI-YrotNa1_rqI8cYYDc3INCKXrzcMBsjbrgrVmNwtdIZLr7nbmlx8ytrEwqJinZmKdOtfB_4N2eiPQlfnm4wrvIcvAQLCwFxQhZ586m686a8SxgCiQ_S8NnF7l2X7OIE9czawR_0CKpS3_VEDCLeUyrIuGsVzVRIsPVYU9k4T_OFpY1KibAW1V8_JVLa1wjvMbRQ1YJdbln7ZnzxRimbWF-7z5cGj20yU6SwanEaP-AOXqNjC86H6W9YBBrBq5XT4w5XiFixMg-XtKV0lEemNqhAjustbFNTDbbK3tMVfzYoJzrFtmuPj1ZRYkrUKa1HckDy4XXAaxL0qrVl9Sq8Pc3agAWZ8361ZhZqGlTQk17LFI0Uvsrw5VfwAbTnclssrH9AO3U_SeyL0oqM8h_G0Luqhg6FpjBbgAjuqHAvP7v1hsQglPm6xbBlR9j4O6QnzOaBkSYJrYtXxiB5B-jOsO2S6e6gLkj1VhnRUiP2mO8t1UFRKtx1w=w687-h914-no?authuser=0"
+        src="https://i.imgur.com/4VqFv52.gifv"
       />
 
       <div className="space-y-10 px-0 md:px-10">
--- portfolio/components/BackgroundCircles.tsx
+++ portfolio/components/BackgroundCircles.tsx
@@ -12,13 +12,12 @@ function BackgroundCircles({}: Props) {
         opacity: [0.1, 0.2, 0.4, 0.8, 0.1, 1.0],
         borderRadius: ["20%", "20%", "50%", "80%", "20%"],
       }}
-      transition={{ duration: 2.5 }}
+      transition={{ duration: 20 }}
       className="relative flex justify-center items-center"
     >
       <div className="absolute border border-[#333333] rounded-full h-[200px] w-[200px] mt-52 animate-ping" />
       <div className="absolute border border-[#333333] rounded-full h-[300px] w-[300px] mt-52 animate-ping" />
       <div className="absolute border border-[#333333] rounded-full h-[500px] w-[500px] mt-52 animate-ping" />
-      <div className="absolute border border-[#DA9840] opacity-50 rounded-full h-[650px] w-[650px] mt-52 animate-pulse" />
       <div className="absolute border border-[#333333] opacity-20 rounded-full h-[800px] w-[800px] mt-52 animate-ping" />
     </motion.div>
   );
