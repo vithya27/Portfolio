0b2a001b093d072724eb312cbe9d1aefa0502328
vithya27 <vithya.shankar93@hotmail.com>
2023-02-01 09:51:36 +0800

    temporary fix for scroll issue

--- portfolio/components/Experience.tsx
+++ portfolio/components/Experience.tsx
@@ -10,7 +10,7 @@ export default function Experience({}: Props) {
       initial={{ opacity: 0 }}
       whileInView={{ opacity: 1 }}
       transition={{ duration: 1.5 }}
-      className="h-screen flex relative overflow-hidden flex-col text-left md:flex-row max-w-full px-10 justify-evenly mx-auto items-center"
+      className="h-screen flex relative flex-col text-left md:flex-row max-w-full px-10 justify-evenly mx-auto items-center"
     >
       <h3 className="sectionHeader">Experience</h3>
 
--- portfolio/components/ExperienceCard.tsx
+++ portfolio/components/ExperienceCard.tsx
@@ -4,13 +4,13 @@ type Props = {};
 
 export default function ExperienceCard({}: Props) {
   return (
-    <article className="flex flex-col mt-24 sm:mt-28 md:mt-60 h-screen rounded-lg items-center space-y-7 flex-shrink-0 w-[300px] md:w-[600px] xl:w-[900px] snap-center bg-line-blue p-5 hover:opacity-100 opacity-40 cursor-pointer transition-opacity overflow-y-auto">
+    <article className="flex flex-col overflow-y-auto mt-24 sm:mt-28 md:mt-60 h-screen rounded-lg items-center space-y-7 flex-shrink-0 w-[300px] md:w-[600px] xl:w-[900px] snap-center bg-line-blue p-5 hover:opacity-100 opacity-40 cursor-pointer transition-opacity">
       <motion.img
         initial={{ y: -100, opacity: 0 }}
         whileInView={{ opacity: 1, y: 0 }}
         transition={{ duration: 1.2 }}
         viewport={{ once: true }}
-        className="w-32 h-32 rounded-full xl:w-[200px] xl:h-[200px] object-cover object-center"
+        className="w-28 h-28 rounded-full xl:w-[200px] xl:h-[200px] object-cover object-center"
         src="https://seeklogo.com/images/G/general-assembly-logo-D5C634F07A-seeklogo.com.png"
         alt=""
       />
@@ -22,7 +22,7 @@ export default function ExperienceCard({}: Props) {
         </p>
         <p className="uppercase py-5 text-muted-blue">2022 - Present</p>
 
-        <ul className="list-disc space-y-4 ml-5 text-lg text-justify">
+        <ul className="list-disc space-y-4 ml-5 text-sm text-justify">
           <li>
             Collaborate with Lead Instructor for Software Engineering to guide a
             cohort of 15 students through a three-month rigorous journey towards
